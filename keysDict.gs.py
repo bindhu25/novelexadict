cce20ba6e44c0021891087e8b9ce6b8a387841be
bindhu25 <85682419+bindhu25@users.noreply.github.com>
2021-06-10 17:04:28 +0530

    Add files via upload

---
+++ keysDict.gs.py
@@ -0,0 +1,101 @@
+"""
+Novelexa project
+
+Keywords
+
+
+"""
+
+
+keys_ActionGS={"author":["Spch","The author of the famous book series Geronimo Stilton is Elisabetta Dami."],             
+             "characters":["Spch","The main characters of Geronimo Stilton are Geronimo Stilton,Thea Stilton,Trap Stilton and Benjimin Stilton."],             
+             "famous quote":["Spch","The famous quote from Geronimo Stilton is 'Loving means sharing what you have however big or small it may be!'"],             
+             "genre":["Spch","It's children's literature"],             
+             "number":["Spch","There are 207 books of Geronimo Stilton in total"],             
+             "plot":["Spch","In the series, the title character is an anthropomorphic mouse who lives in the fictional New Mouse City on fictional Mouse Island. A best-selling author, Geronimo Stilton works as a journalist and editor for the fictional newspaper The Rodent's Gazette.He has a younger sister named Thea Stilton,a cousin named Trap Stilton,and a favourite nephew, nine-year-old Benjamin Stilton and also Petunina Pretty Paws. Geronimo is a nervous, mild-mannered mouse who would like nothing better than to live a quiet life, but he keeps getting involved in faraway adventures with Thea, Trap, and Benjamin, and sometimes Aunt Sweetfur. The books are written as though they are autobiographical adventure stories."],            
+             "best selling":["Spch","The Kingdom of Fantasy which is a special edition is the best selling book of Geronimo Stilton."],             
+             "TV series":["Spch","The New Adventures of Geronimo Stilton is the TV seies basedd on Geronimo Stilton."],             
+             "year":["Spch","In the year 2000 the first book of Geronimo Stilton was published."],            
+             "Thea Stilton":["Spch","Thea Stilton is a special correspondent for The Rodent's Gazette, Mouse Island's most famouse newspaper. A graduate of Mouseford Academy, Ms. Stilton loves traveling and adventures."],             
+             "Thea Sisters":["Spch","The Thea Sisters are a group of young mouselets who currently attend Mouseford Academy. The group consists of Nicky, Violet, Colette, Paulina, and Pamela. They got their group's name from Thea Stilton, who teaches them in their journalism courses and serves as their mentor."],             
+             "Geronimo Stilton":["Spch","Geronimo Stilton is an anthropomorphic mouse who lives in the fictional New Mouse City on fictional Mouse Island. A best-selling author, Geronimo Stilton works as a journalist and editor for the fictional newspaper The Rodent's Gazette."],            
+             "first book":["Spch","Lost Treasure of the Emerald Eye is the first book published."],            
+             "sold":["Spch","The series has sold over 180 million copies worldwide, and with 249 editions it is one of the best-selling book series ever written.The books have been translated into 49 languages, with more translations to come."],             
+             "publisher":["ImgSpch","Edizioni Piemme Scholastic Corporation is the publisher of Geronimo Stilton."],             
+             "language":["ImgSpch","Geronimo Stilton was originally written in Italian."],             
+             "latest":["Spch","The Garbage Thief is the latest book of Geronimo Stilton, it was published on 21 September 2021."],             
+             "moral":["Spch","Geronimo Stilton's behavior is based on universal ethical values, like being approachable for others, the desire to do good, the commitment to grow and improve oneself.Additionally, Geronimo is brought to life with a healthy optimism: he is never discouraged in negative situations."],           
+             "age group":["Spch","The books are intended for readers in the 8–12 year-old age range."],          
+             "sister":["Spch","Thea Stilton is Geronimo's younger sister and the special correspondent for The Rodent's Gazette."]}
+             
+
+keys_ActionPJ={"series":["There are 5 books with a sequel series called the Heroes of Olympus""PJO"],              
+              "author":["The author of the Percy Jackson series is Rick Riordan""Rick"],               
+              "genre":["The genres of the Percy Jackson series are fantasy, Greek Mythology, and young adult fiction."],               
+              "main characters":["Percy Jackson, Annabeth Chase, Grover Underwood, Poseidon, Chiron, and Mr. D are some fo the few characters in the series."],               
+              "movie":["There are two movies on the series with the names Percy Jackson and The Lightning Thief and Percy Jackson and the Sea of Monsters."],               
+              "fact":["Mostly all demigods have ADHD or/and Dyslexia."],               
+              "trivia":["All children of Poseidon can talk to equestrial animals or horses as Poseidon created them."],               
+              "poseidon":["The Greek god of the sea is known as Poseidon."],               
+              "zeus":["The greek god of lightning is knwon as the King of Olympus and his name is Zeus."],               
+              "hades":["The greek god of death is known as Hades."],
+              "olympians":["The council of greek gods are knwon as the Olympians."],
+              "olympus":["Olympus is knwon as the home of the gods and is also a mountain on which it is believed the gods once lived on."],
+              "buy":["The Percy Jackson series can be bought on Amazon.in or can be purchased on the official readriordan.com website from one of your local stores."]}
+
+
+keys_ActionHP={ "Name Harry Potter's parents":["Spch","James and Lily Potter are Harry Potter's parents"],
+               "What is the name of Harry Potter's owl?":["Spch","Hedwig is the name of Harry Potter's owl"],
+                "Who is Draco Malfoy's dad?":["Spch","Lucius Malfoy is Draco Malfoy's dad"],
+                "Name Ron Weasley's parents.":["Spch","Arthur and Molly are the names of Ron Weasley's parents"],
+                "Name Ron Weasley's brothers and sister.":["Spch","Name Ron Weasley's brothers and sister."],
+                "Name Ron Weasley's pet rat.":["Spch","Name Ron Weasley's pet rat."],
+                "Who did Ron Weasley's pet rat used to belong to?":["Spch","Ron Weasley's pet rat used to belong to His brother Percy"],
+                "Name Luna Lovegood's father.":["Spch","Name Luna Lovegood's father is Xenophillius Lovegood"],
+                "Why did the house-elf Dobby appear at Harry's home?":["Spch","He came to warn Harry about the plot to reopen the Chamber of Secrets"],
+                "Who was the first goblin Harry ever met?":["Spch","Griphook was the first goblin Harry ever met"],
+                "What creature is Aragog?":["Spch","Acromantula is the Aragog creature"],
+                "What was Hagrid's pet dog called?":["Spch","Hagrid's pet dog is called Fang"],
+                "Name the centaur who taught at Hogwarts.":["Spch","Professor Firenze is the centaur who taught at Hogwarts."],
+                "Who were the four founders of Hogwarts?":["Spch","Godric Gryffindor, Helga Hufflepuff, Rowena Ravenclaw and Salazar Slytherin were the four founders of Hogwarts"],
+                "What animal could Peter Pettigrew turn into":["Spch","Ron's pet rat, Scabbers animal could Peter Pettigrew turn into"],
+                "What was the name of Lord Voldemort's loyal snake?":["Spch","Nagini was the name of Lord Voldemort's loyal snake"],
+                "Who killed Dobby by throwing a knife at him?":["Spch","Bellatrix Lestrange killed Dobby by throwing a knife at him"],
+                "who is the writer of harry potter?":["Spch","J. K. Rowling is the writer of harry potter?"],
+               # keywords
+                "Wand":["Spch","A wand is a thin, light-weight rod that is held with one hand, and is traditionally made of wood, but may also be made of other materials, such as metal or plastic. Wands are distinct from scepters, which have a greater thickness, are held differently, and have a relatively large top ornament on them"],
+                "Muggle":["Spch","In J. K. Rowling's Harry Potter series, a Muggle is a person who lacks any sort of magical ability and was not born in a magical family."],
+                "Galleon":["Spch","A Galleon or Gold-Galleon (ʛ) is the most valued coin of the wizarding currency used in Britain. One Galleon is equal to 17 Sickles or 493 Knuts. Galleons are made of gold."],
+                "Poltergeist.":["Spch","The name 'poltergeist' is German in origin, and roughly translates as 'noisy ghost', although it is not, strictly speaking, a ghost at all. The poltergeist is an invisible entity that moves objects, slams doors and creates other audible, kinetic disturbances.."],
+                "Herbology":["Spch","Herbology was the study of magical and mundane plants and fungi, making it the wizarding equivalent to botany."],
+                "Remembrall":["Spch","A Remembrall is a magical large marble-sized glass ball that contains smoke which turns red when its owner or user has forgotten something. It turns clear once whatever was forgotten is remembered."],
+                "Centaurs":["Spch","A Centaur was a magical creature whose head, torso, and arms appeared to be human and were joined to a horse's body. They were, however, their own individual species, and thus were not half-breeds. They also had a rich history and were known for being naturally talented in Healing magic, Divination and Astronomy."],
+                "Transfiguration":["Spch","Transfiguration is the family of magical spells that are used for changing objects from one type of thing into another. At Hogwarts, Transfiguration is taught by Professor Minerva McGonagall."],
+                "Seeker":["Spch","Seeker is a position in the wizarding sport of Quidditch. ... The goal of the Seeker is to catch the Golden Snitch. They play a crucial role in Quidditch, as a game does not end until the Seeker catches the Snitch. A team whose Seeker catches the Snitch receives 150 points, which almost always wins the match for that team."]}
+
+
+keys_ActionND={ "Nancy Drew":["Spch","Nancy Drew is a fictional character, a sleuth in an American mystery series. The character first appeared in 1930. The books are ghostwritten by a number of authors and published under the collective pseudonym Carolyn Keene."],
+               "Publisher":["Spch","Nancy Drew is a mystery series created by publisher Edward Stratemeyer"],
+                "When did the character first appear?":["Spch","The character first appeared in 1930."],
+                "Which was the first Nancy Drew mystery?":["Spch","The Secret of the Old Clock was the first Nancy Drew mystery."],
+                "Who wrote the frist volume of Nancy Drew?":["Spch","The Secret of the Old Clock is the first volume in the Nancy Drew Mystery Stories series written under the pseudonym Carolyn Keene."],
+                "Name Ron Weasley's pet rat.":["Spch","Name Ron Weasley's pet rat."],
+                "Who did Ron Weasley's pet rat used to belong to?":["Spch","Ron Weasley's pet rat used to belong to His brother Percy"],
+                "Who was Nancy Drew?":["Spch","Nancy Drew is a 16-year-old high school graduate (her age was changed to 18 in the 1959 rewrite)."],
+                "When was the first volume of Nancy Drew series published and who re-wrote it?":["Spch","It was first published on April 28, 1930, and rewritten in 1959 by Harriet Stratemeyer Adams."],
+                "Who was Nancy Drew's father":["Spch"," Her father, Carson Drew, is a well-known criminal defense lawyer."],
+                "Where did the drew's reside":["Spch","The Drews reside in River Heights"],
+                "who was Hannah Gruen":["Spch","Hannah Gruen was the housekeeper of Drew's family. In early editions, she is depicted as a mere servant; later in the series, she becomes more of a family member."],
+                "Ranking":["Spch","In 2001, the novel ranked 53rd on Publishers Weekly's list of the all-time best-selling hardcover children's books in English, having sold about 2.7 million copies."],
+                "Summary of the 1930 edition":["Spch","Sixteen-year-old Nancy Drew wishes to help the Turners, who are struggling relatives of the recently deceased Josiah Crowley, by finding a missing will that can give them claim to Crowely's estate. Aided along the way by chum Helen Corning, she becomes interested in the case because she dislikes Crowley's snobbish nouveau-riche social-climbing heirs presumptive, the Tophams."],
+                "Nationality of Nancy Drew":["Spch","American"],
+                "Translations":["Spch"," the books have been translated into over 45 languages."],
+                "Featured in...?":["Spch","Nancy Drew is featured in five films, three television shows, and a number of popular computer games"],
+
+                "exit":["exit","Good Bye and Keep reading my friend!!"],
+                "close":["exit","Have a nice Day!"]}            
+                                                 
+
+
+             
+                    
